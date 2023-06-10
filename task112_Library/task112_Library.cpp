5cb8a0fcb03a1d358cd252441411c5aed5ff0550
SergeyZheleznov <jeleznov_sv@mail.ru>
2023-06-10 11:11:04 +0300

    first commit

---
+++ task112_Library/task112_Library.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "task112_Library.h"
+
+namespace task112_Library
+{
+	std::string Greeter::get_greet(std::string name) { return name; };
+}
---
+++ task112_Library/task112_Library.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+namespace task112_Library
+{
+	class Greeter
+	{
+	public:
+		std::string get_greet(std::string name);
+	};
+}
